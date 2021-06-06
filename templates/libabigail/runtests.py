7f95f8214c4009d211ec11d073aac65786552b29
vsoch <vsoch@users.noreply.github.com>
2021-06-06 13:11:30 -0600

    updating mpich.yaml to have additional files to run abicompat
    We cant move any further at this point because mpich needs to be built
    with debug. There is neither a variant (I opened a PR) nor is there
    a global debug
    
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- templates/libabigail/runtests.py
+++ templates/libabigail/runtests.py
@@ -27,6 +27,12 @@ def find_install_paths(package):
         paths[spec.strip()] = path.strip()
     return paths
 
+def create_outdir(filename):
+    """Create the output directory for a given filename
+    """
+    out_dir = os.path.dirname(filename)
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
 
 def run(cmd):
     """
@@ -41,9 +47,23 @@ def main():
     Entrypoint to run tests.
     """
     package = "{{ package.name }}"
+    here = os.getcwd()
+    envpath = os.environ["PATH"]
+
+    # Run extra commands first
+    lookup = find_install_paths(package)
+{% if package.run %}
+    for spec, path in lookup.items():
+
+        # If there is a set of commands to run, do it first
+        os.chdir(path)
+        # Add package bin to the path and run extra commands
+        os.environ["PATH"] = "%s/bin:%s" % (os.getcwd(), envpath)
+        {% for run in package.run %}run("{{ run }}")
+        {% endfor %}
+        os.chdir(here){% endif %}
 
     # spec names -> paths
-    lookup = find_install_paths(package)
     for spec, path in lookup.items():
         spec, spec_version = spec.split("@", 1)
         {% for lib in package.libs %}print("Testing {{ lib }} with abidw")
@@ -60,25 +80,35 @@ def main():
         if not os.path.exists(lib):
             print("Skipping %s, does not exist." % lib)
             continue
-            
+                    
         # We don't need output here, lazy way to do it
-        run("time -p abidw {% for include in package.include %} --hd %s/{{ include }} {% endfor %} %s --out-file %s/{{ lib }}.xml > %s/{{ lib }}.xml.log 2>&1" % ({% for include in package.include %}path, {% endfor %}lib, out_dir, out_dir))
+        run("time -p abidw {% for include in package.headers %} --hd %s/{{ include }} {% endfor %} %s --out-file %s/{{ lib }}.xml > %s/{{ lib }}.xml.log 2>&1" % ({% for include in package.headers %}path, {% endfor %}lib, out_dir, out_dir))
 
         for spec2, path2 in lookup.items():
             spec2, spec2_version = spec2.split("@", 1)
             print("Comparing %s versions %s and %s {{ lib }} with abidiff" %(spec, spec_version, spec2_version))
             out_file = "/results/{{ tester.name }}/{{ tester.version }}/{{ package.name }}/diff/%s-%s" % (spec_version, spec2_version)
-            out_dir = os.path.dirname(out_file)
-            if not os.path.exists(out_dir):
-                os.mkdir(out_dir)
- 
+            create_outdir(out_file)
+
             # Only run if the named library exists
             lib2 = "%s/{{ lib }}" % path2
             if not os.path.exists(lib2):
                 print("Skipping %s, does not exist.")
                 continue
 
-            run("time -p abidiff {% for include in package.include %} --hd1 %s/{{ include }} --hd2 %s/{{ include }} {% endfor %} %s %s > %s > %s.log" %({% for include in package.include %}path, path2, {% endfor %}lib, lib2, out_file, out_file))
+            run("time -p abidiff {% for include in package.headers %} --hd1 %s/{{ include }} --hd2 %s/{{ include }} {% endfor %} %s %s > %s > %s.log" %({% for include in package.headers %}path, path2, {% endfor %}lib, lib2, out_file, out_file))
+
+            # If we have bins to run abicompat with
+            {% for binary in package.bins %}
+            bin1 = os.path.join(path, "{{ binary }}") 
+
+            # We can only run abicompat if it exists
+            if os.path.exists(bin1):
+
+                out_file = "/results/{{ tester.name }}/{{ tester.version }}/{{ package.name }}/compat/%s-%s" % (spec_version, spec2_version)
+                create_outdir(out_file)
+                run("time -p abicompat %s %s %s > %s > %s.log" % (bin1, lib, lib2, out_file, out_file))
+                {% endfor %}
         {% endfor %}
 
 
