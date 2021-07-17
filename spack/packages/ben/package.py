40ce491760e6d11d9c17e8e9c7b2c9d79f74268b
vsoch <vsoch@users.noreply.github.com>
2021-07-17 14:44:13 -0600

    fixing ben test
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- spack/packages/ben/package.py
+++ spack/packages/ben/package.py
@@ -9,11 +9,14 @@ from spack import *
 class Ben(MakefilePackage):
     homepage = "https://github.com/buildsi/build-abi-test-ben"
     url = "https://github.com/buildsi/build-abi-test-ben/archive/refs/tags/2.0.0.tar.gz"
+    git = "https://github.com/buildsi/build-abi-test-ben.git"
 
+
+    version('main', branch='main')
     version('1.0.0', sha256='28d85b076c7127934b1127c1695bbdc16e59ec5a42def68478c938835fbe114f')
     version('2.0.0', sha256='d913cf8de6ed92c836388bc7a492e0f975e589bab7616d99607180559d487d72')
 
-    depends_on("gcc@11.1.0")
+#    depends_on("gcc@11.1.0", type="build")
 
     def install(self, spec, prefix):
         mkdir(prefix.bin)
